ca8108de743268806e4f27402147384524f9aa16
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-04 09:57:39 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim YourMessage As String
+        Dim Birthyear As Integer
+        YourMessage = "HAPPY BIRTHDAY! HOW OLD ARE YOU NOW?"
+
+        MsgBox(YourMessage)
+        Birthyear = Val(InputBox("Birth Year"))
+        MsgBox("YOU ARE NOW" & 2023 - Birthyear & "YEARS OLD")
+
+    End Sub
+End Class
